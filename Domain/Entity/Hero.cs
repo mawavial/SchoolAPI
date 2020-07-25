8cdfc5ba43c061136bed0edb235c5b3ac9f843ca
mawavial <panzerkovic@gmail.com>
2020-07-25 03:20:10 -0300

    Creates basic DDD structure

---
+++ Domain/Entity/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entity
+{
+    public class Hero
+    {
+        long Id { get; set; }
+        string Name { get; set; }
+    }
+}
